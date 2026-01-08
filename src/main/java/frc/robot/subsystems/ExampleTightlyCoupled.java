216fc7decc304c5f7b0a08056c68b80163fe9c2a
Hayden Schroeder <hschroeder0317@gmail.com>
2026-01-07 21:13:31 -0600

    Create smart controller config

---
+++ src/main/java/frc/robot/subsystems/ExampleTightlyCoupled.java
@@ -0,0 +1,115 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import org.littletonrobotics.junction.inputs.LoggableInputs;
+
+import edu.wpi.first.math.controller.SimpleMotorFeedforward;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.subsystems.LoggingSubsystem.LoggedSubsystem;
+import yams.gearing.GearBox;
+import yams.gearing.MechanismGearing;
+import yams.motorcontrollers.SmartMotorControllerConfig;
+import yams.motorcontrollers.SmartMotorControllerConfig.ControlMode;
+import yams.motorcontrollers.SmartMotorControllerConfig.MotorMode;
+import yams.motorcontrollers.SmartMotorControllerConfig.TelemetryVerbosity;
+import frc.robot.model.ExampleSubsystemLogAutoLogged;
+
+import static edu.wpi.first.units.Units.Amps;
+import static edu.wpi.first.units.Units.DegreesPerSecond;
+import static edu.wpi.first.units.Units.DegreesPerSecondPerSecond;
+import static edu.wpi.first.units.Units.Seconds;
+
+import edu.wpi.first.math.controller.SimpleMotorFeedforward;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import yams.mechanisms.SmartMechanism;
+import yams.motorcontrollers.SmartMotorControllerConfig;
+import yams.motorcontrollers.SmartMotorControllerConfig.ControlMode;
+import yams.motorcontrollers.SmartMotorControllerConfig.MotorMode;
+import yams.motorcontrollers.SmartMotorControllerConfig.TelemetryVerbosity;
+
+
+
+// Implements LoggedSubsystem to enable logging
+public class ExampleTightlyCoupled extends SubsystemBase implements LoggedSubsystem { 
+
+  // Create an instance of the log model
+  private ExampleSubsystemLogAutoLogged exampleSubsystemAutoLogged = new ExampleSubsystemLogAutoLogged();
+
+  /** Creates a new ExampleSubsystem. */
+  public ExampleTightlyCoupled() {
+
+    // Example SmartMotorControllerConfig setup
+    private SmartMotorControllerConfig smcConfig = new SmartMotorControllerConfig(this)
+    .withControlMode(ControlMode.CLOSED_LOOP)
+    .withClosedLoopController(50, 0, 0, DegreesPerSecond.of(90), DegreesPerSecondPerSecond.of(45))
+    .withSimClosedLoopController(50, 0, 0, DegreesPerSecond.of(90), DegreesPerSecondPerSecond.of(45))
+    // Feedforward Constants
+    .withFeedforward(new SimpleMotorFeedforward(0, 0, 0)) // No feedforward
+    .withSimFeedforward(new SimpleMotorFeedforward(0, 0, 0)) // No feedforward
+    // Telemetry name and verbosity level
+    .withTelemetry("ShooterMotor", TelemetryVerbosity.HIGH)
+    // Gearing from the motor rotor to final shaft.
+    // In this example GearBox.fromReductionStages(3,4) is the same as GearBox.fromStages("3:1","4:1") which corresponds to the gearbox attached to your motor.
+    // You could also use .withGearing(12) which does the same thing.
+    .withGearing(new MechanismGearing(GearBox.fromReductionStages(3, 4)))
+    // Motor properties to prevent over currenting.
+    .withMotorInverted(false)
+    .withIdleMode(MotorMode.COAST)
+    .withStatorCurrentLimit(Amps.of(40));
+
+  }
+
+  /**
+   * Example command factory method.
+   *
+   * @return a command
+   */
+  public Command exampleMethodCommand() {
+    // Inline construction of command goes here.
+    // Subsystem::RunOnce implicitly requires `this` subsystem.
+    return runOnce(
+        () -> {
+          /* one-time action goes here */
+        });
+  }
+
+  /**
+   * An example method querying a boolean state of the subsystem (for example, a digital sensor).
+   *
+   * @return value of some boolean subsystem state, such as a digital sensor.
+   */
+  public boolean exampleCondition() {
+    // Query some boolean state, such as a digital sensor.
+    return false;
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+
+  @Override
+  public void simulationPeriodic() {
+    // This method will be called once per scheduler run during simulation
+  }
+
+  /**
+ * Updates and returns the loggable inputs for this subsystem.
+ * <p>
+ * This method populates the {@link ExampleSubsystemLogAutoLogged} instance with current
+ * subsystem data (e.g., setting {@code exampleValue} to 6.7) and returns it for logging purposes.
+ * It is required by the {@link LoggedSubsystem} interface to ensure telemetry data is recorded every loop.
+ *
+ * @return The updated {@link LoggableInputs} object containing the current state of the subsystem.
+ */
+  @Override
+  public LoggableInputs log() {
+    exampleSubsystemAutoLogged.exampleValue = 6.7;
+    return exampleSubsystemAutoLogged;
+  }
+}
