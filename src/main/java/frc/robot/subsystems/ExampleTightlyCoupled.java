96829a795c0959967f10ab7186e5b89918712eaf
Hayden Schroeder <hschroeder0317@gmail.com>
2026-01-07 21:26:29 -0600

    ExampleTightlyCoupled

--- src/main/java/frc/robot/subsystems/ExampleTightlyCoupled.java
+++ src/main/java/frc/robot/subsystems/ExampleTightlyCoupled.java
@@ -22,16 +22,26 @@ import static edu.wpi.first.units.Units.Amps;
 import static edu.wpi.first.units.Units.DegreesPerSecond;
 import static edu.wpi.first.units.Units.DegreesPerSecondPerSecond;
 import static edu.wpi.first.units.Units.Seconds;
+import static edu.wpi.first.units.Units.Inches;
+import static edu.wpi.first.units.Units.RPM;
 
-import edu.wpi.first.math.controller.SimpleMotorFeedforward;
-import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import yams.mechanisms.SmartMechanism;
-import yams.motorcontrollers.SmartMotorControllerConfig;
-import yams.motorcontrollers.SmartMotorControllerConfig.ControlMode;
-import yams.motorcontrollers.SmartMotorControllerConfig.MotorMode;
-import yams.motorcontrollers.SmartMotorControllerConfig.TelemetryVerbosity;
+import yams.mechanisms.config.FlyWheelConfig;
+import yams.mechanisms.velocity.FlyWheel;
 
+
+
+import static edu.wpi.first.units.Units.Feet;
+import static edu.wpi.first.units.Units.Pounds;
+
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.SparkMax;
+
+import edu.wpi.first.math.controller.ArmFeedforward;
+import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.units.measure.AngularVelocity;
+import yams.motorcontrollers.SmartMotorController;
+import yams.motorcontrollers.local.SparkWrapper;
 
 
 // Implements LoggedSubsystem to enable logging
@@ -39,9 +49,6 @@ public class ExampleTightlyCoupled extends SubsystemBase implements LoggedSubsys
 
   // Create an instance of the log model
   private ExampleSubsystemLogAutoLogged exampleSubsystemAutoLogged = new ExampleSubsystemLogAutoLogged();
-
-  /** Creates a new ExampleSubsystem. */
-  public ExampleTightlyCoupled() {
 
     // Example SmartMotorControllerConfig setup
     private SmartMotorControllerConfig smcConfig = new SmartMotorControllerConfig(this)
@@ -61,8 +68,51 @@ public class ExampleTightlyCoupled extends SubsystemBase implements LoggedSubsys
     .withMotorInverted(false)
     .withIdleMode(MotorMode.COAST)
     .withStatorCurrentLimit(Amps.of(40));
+    
+    // Vendor motor controller object
+    private SparkMax spark = new SparkMax(4, MotorType.kBrushless);
 
-  }
+    // Create our SmartMotorController from our Spark and config with the NEO.
+    private SmartMotorController sparkSmartMotorController = new SparkWrapper(spark, DCMotor.getNEO(1), smcConfig);
+
+    private final FlyWheelConfig shooterConfig = new FlyWheelConfig(sparkSmartMotorController)
+    // Diameter of the flywheel.
+    .withDiameter(Inches.of(4))
+    // Mass of the flywheel.
+    .withMass(Pounds.of(1))
+    // Maximum speed of the shooter.
+    .withUpperSoftLimit(RPM.of(1000))
+    // Telemetry name and verbosity for the arm.
+    .withTelemetry("ShooterMech", TelemetryVerbosity.HIGH);
+
+    // Shooter Mechanism
+    private FlyWheel shooter = new FlyWheel(shooterConfig);
+
+  /** Creates a new ExampleSubsystem. */
+  public ExampleTightlyCoupled() {}
+
+  /**
+   * Gets the current velocity of the shooter.
+   *
+   * @return Shooter velocity.
+   */
+  public AngularVelocity getVelocity() {return shooter.getSpeed();}
+
+  /**
+   * Set the shooter velocity.
+   *
+   * @param speed Speed to set.
+   * @return {@link edu.wpi.first.wpilibj2.command.RunCommand}
+   */
+  public Command setVelocity(AngularVelocity speed) {return shooter.setSpeed(speed);}
+
+  /**
+   * Set the dutycycle of the shooter.
+   *
+   * @param dutyCycle DutyCycle to set.
+   * @return {@link edu.wpi.first.wpilibj2.command.RunCommand}
+   */
+  public Command set(double dutyCycle) {return shooter.set(dutyCycle);}
 
   /**
    * Example command factory method.
@@ -91,11 +141,13 @@ public class ExampleTightlyCoupled extends SubsystemBase implements LoggedSubsys
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+    shooter.updateTelemetry();
   }
 
   @Override
   public void simulationPeriodic() {
     // This method will be called once per scheduler run during simulation
+    shooter.simIterate();
   }
 
   /**
